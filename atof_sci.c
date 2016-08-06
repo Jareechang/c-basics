c61833a06fbba5c58591756a2e1034e64f268c1f
Jerry Chang <therealjerrycan@gmail.com>
2016-08-06 15:13:06 -0700

    fine tuning string to float calculator — added scientific notation (e or E)

--- atof_sci.c
+++ atof_sci.c
@@ -4,20 +4,21 @@
 
 double atof (char s[]); 
 int is_digit(int digit);
+int get_sign(char s[]);
 
 int main() 
 {
-    printf("atof: %4.2f\n", atof("-311.23"));
+    printf("atof: %4.4f\n", atof("-3.23e-2"));
     return 0;
 }
 
 double atof (char s[])
 {
     // Example -10.0
-    double res, sig, dec_figures, m;
-    int i, power, sign;
+    double res, sig, decimal_figures, m;
+    int i, power, sign, e_sign, e_power;
     power = m = 10;
-    i = sig = dec_figures = 0;
+    i = sig = decimal_figures = e_power = 0;
 
     sign = (s[i] == '-') ? -1 : 1;
     if(s[i] == '-' || s[i] == '+') {
@@ -35,13 +36,29 @@ double atof (char s[])
 
     if(is_digit(s[i])) {
         while(is_digit(s[i])) {
-            dec_figures = dec_figures + ((s[i] - '0') / m);
+            decimal_figures = decimal_figures + ((s[i] - '0') / m);
             m *= 10;
             i++;
         }
     }
 
-    return sign * (sig + dec_figures);
+    if(s[i++] == 'e' || s[i++] == 'E') {
+        e_sign = get_sign(&s[i]);
+        if(s[i] == '-' || s[i] == '+') {
+            i++;
+        }
+        while(is_digit(s[i]) && s[i] != '\0') {
+            e_power = (e_power * power) + (s[i] - '0');
+            i++;
+        }
+    }
+
+    return (sign * (sig + decimal_figures)) * pow(10, e_sign * e_power);
+}
+
+int get_sign(char s[]) 
+{
+    return (*s == '-') ? -1 : 1;
 }
 
 int is_digit(int digit) 
