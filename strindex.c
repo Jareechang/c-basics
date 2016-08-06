5fa19c696b3300185b1a51f755e1e4c24a2dd0ba
Jerry Chang <therealjerrycan@gmail.com>
2016-08-05 18:02:17 -0700

    updated strindex (updated from the rightmost occurence)

---
+++ strindex.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int strindex(char s[], char t[]);
+
+int main()
+{
+    printf("strindex(\"abcabc\", \"ab\") ==> %d\n", strindex("cdfcdf", "cd"));
+    return 0;
+}
+
+int strindex(char s[], char t[])
+{
+    int i, j, k;
+
+    int s_len = strlen(s);
+    int t_len = strlen(t);
+
+    for(i = s_len - t_len; i >= 0; i--) {
+        for(j = i, k = 0; t[k] != '\0' && s[i] == t[k]; j++, k++)
+            ;
+        if(j > 0 && t[j] == '\0')  
+            return i;
+    }
+    return -1;
+}
+
