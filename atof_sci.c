ce6fe14d52b0f68bc54a3cb4620d2a8c413c2a87
Jerry Chang <therealjerrycan@gmail.com>
2016-08-07 17:21:52 -0700

    refactored code (step 1)

--- atof_sci.c
+++ atof_sci.c
@@ -6,59 +6,68 @@ double atof (char s[]);
 int digit(int digit);
 int get_sign(char s[]);
 int scientific_notation(char s[]);
+int increment_index(int i, char s[]);
+int skip_index(char s[]);
 
 int main() 
 {
-    printf("atof: %4.4f\n", atof("-3.23e-2"));
+    printf("atof: %4.4f\n", atof("322.404"));
     return 0;
 }
 
 double atof (char s[])
 {
     // Example -10.0
-    double res, num, decimal_figures, m;
+    double num, decimal_figures, m;
     int i, power, sign, e_sign, e_power;
     double scientific_figures = 0.0;
 
     power = m = 10;
     i = num = decimal_figures = e_power = 0;
+    int after_decimals = 0;
+    int after_exp = 0;
 
     sign = get_sign(&s[i]);
 
-    if(s[i] == '-' || s[i] == '+') {
-        i++;
-    }
-
-    while(digit(s[i])) {
-        num = (num * power) + (s[i] - '0');
-        i++;
-    }
-
-    if(s[i] == '.') {
-        i++;
-    }
-
-    if(digit(s[i])) {
-        while(digit(s[i])) {
+    while(s[i] != '\0') {
+        if(skip_index(&s[i])) {
+            if(s[i] == '.') {
+                after_decimals = 1;
+            }
+            if(after_exp) {
+                e_sign = get_sign(&s[i]);
+            }
+        } else if(digit(s[i]) && !after_decimals) {
+            num = (num * power) + (s[i] - '0');
+        } else if(digit(s[i]) && !after_exp) {
             decimal_figures = decimal_figures + ((s[i] - '0') / m);
             m *= 10;
-            i++;
+        } else if(scientific_notation(&s[i])) {
+            after_exp = 1;
+        } else if(after_exp) {
+            while(digit(s[i])) {
+                e_power = (e_power * power) + (s[i] - '0');
+                i++;
+            }
+            scientific_figures = pow(10, e_sign * e_power); /* Calculate the scientific figures */
         }
+        i++;
     }
 
-    if(scientific_notation(&s[i++])) {
-        e_sign = get_sign(&s[i]);
-        if(s[i] == '-' || s[i] == '+') {
-            i++;
-        }
-        while(digit(s[i])) {
-            e_power = (e_power * power) + (s[i] - '0');
-            i++;
-        }
-        scientific_figures = pow(10, e_sign * e_power); /* Calculate the scientific figures */
-    }
+    return (sign * (num + decimal_figures)) * (after_exp ? scientific_figures : 1);
+}
 
-    return (sign * (num + decimal_figures)) * scientific_figures;
+int skip_index(char s[])
+{
+    return (*s == '-' || *s == '+' || *s == '.');
+}
+
+int increment_index(int i, char s[])
+{
+    if(*s == '-' || *s == '+' || *s == '.') {
+        return i + 1;
+    }    
+    return i;
 }
 
 int get_sign(char s[]) 
