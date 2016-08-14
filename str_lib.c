e1693ef0fc245c95af92d411dd2c430a8acddfcb
Jerry Chang <therealjerrycan@gmail.com>
2016-08-14 23:13:03 +0000

    added str_cat method (concatenate n-chars from one string to another)

--- str_lib.c
+++ str_lib.c
@@ -10,12 +10,26 @@ void s_copy(char *s, char *t, int n)
         ;
 }
 
+/* s_cat: concatenate n char from t to s */
+void s_cat(char *s, char *t, int n)
+{
+    while(*++s != '\0') /* find end of first str */
+        ;
+    s_copy(t, s, n);
+}
+
 int main()
 {
     char s1[MAXLINE] = "hello";
-    char s2[MAXLINE] = "";
+    char s2[MAXLINE] = "123";
     int n = 3;
-    s_copy(s1, s2, n);
-    printf("original str: %s, copied str: %s, number of letters copied: %d\n", s1, s2, n);
+    /* testing for s_copy  */
+
+    /*s_copy(s1, s2, n);*/
+    /*printf("original str: %s, copied str: %s, number of letters copied: %d\n", s1, s2, n);*/
+
+    /* testing for s_cat */
+    s_cat(s1, s2, n);
+    printf("combined str: %s strings to combine: %d\n", s1, n);
     return 0;
 }
