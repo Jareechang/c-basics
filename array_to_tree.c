5852df296d8c2971683c6ab51c28f5c6355de890
Jerry Chang <therealjerrycan@gmail.com>
2016-08-21 05:31:07 +0000

    added free_tree (to deallocate memory - double checked with valgrind)

--- array_to_tree.c
+++ array_to_tree.c
@@ -10,17 +10,19 @@ struct BinaryTree {
 
 struct BinaryTree *create_tree(char **s, int start, int end);
 struct BinaryTree *tree_alloc(void);
+void print_tree(struct BinaryTree *root);
+void free_tree(struct BinaryTree *root);
 char *str_dup(char **s, int mid);
 
 int main()
 {
     char *s[] = { "A", "B", "C", "D", "E", "F", "G" };
     int size = 7;
-
     int end = (size - 1);  
     int start = 0;
     struct BinaryTree *letters = create_tree(s, start, end);
-    printf("value: %s\n", letters->right->value);
+    print_tree(letters);
+    free_tree(letters);
     return 0;
 }
 
@@ -33,6 +35,7 @@ struct BinaryTree *create_tree(char **s, int start, int end)
     Tree->value = str_dup(s, mid);
 
     if(mid == start || mid == end) {
+        Tree->left = Tree->right = NULL;
         return Tree;
     }
     Tree->left =  create_tree(s, start, (mid - 1));
@@ -43,6 +46,25 @@ struct BinaryTree *create_tree(char **s, int start, int end)
 struct BinaryTree *tree_alloc(void)
 {
     return (struct BinaryTree *) malloc(sizeof(struct BinaryTree));
+}
+
+void print_tree(struct BinaryTree *root)
+{
+    if(root != NULL) {
+        printf("%s\n", root->value);
+        print_tree(root->left);
+        print_tree(root->right);
+    }
+}
+
+void free_tree(struct BinaryTree *root) 
+{
+    if (root != NULL) {
+        free(root->value);
+        free_tree(root->left);
+        free_tree(root->right);
+        free(root);
+    }
 }
 
 char *str_dup(char **s, int mid)
